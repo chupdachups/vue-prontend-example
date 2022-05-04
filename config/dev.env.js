b32b7e55a86db8649d88c532e386b060c128e050
chupdachups <chupdachups@gmail.com>
2022-05-05 00:15:29 +0900

    update

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  ACCOUNT_API_BASE_URL: '"http://localhost:8070/"',
-  PRODUCT_API_BASE_URL: '"http://localhost:8071/"',
-  RESERVE_API_BASE_URL: '"http://localhost:8072/"'
+  ACCOUNT_API_BASE_URL: '"http://account-service.msa-service.svc.cluster.local:8070/"',
+  PRODUCT_API_BASE_URL: '"http://product-service.msa-service.svc.cluster.local:8071/"',
+  RESERVE_API_BASE_URL: '"http://reserve-service.msa-service.svc.cluster.local:8072/"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,7 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  ACCOUNT_API_BASE_URL: '"http://account-service.msa-service.svc.cluster.local:8070/"',
+  PRODUCT_API_BASE_URL: '"http://product-service.msa-service.svc.cluster.local:8071/"',
+  RESERVE_API_BASE_URL: '"http://reserve-service.msa-service.svc.cluster.local:8072/"'
 }
