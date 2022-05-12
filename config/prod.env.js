2a1ede932ee7e1172dbb030f80401c8f41308d30
chupdachups <chupdachups@gmail.com>
2022-05-12 22:42:28 +0900

    add nginx config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ACCOUNT_API_BASE_URL: '"http://account-service:8070/"',
-  PRODUCT_API_BASE_URL: '"http://product-service:8071/"',
-  RESERVE_API_BASE_URL: '"http://reserve-service:8072/"'
+  ACCOUNT_API_BASE_URL: '"http://account-service.msa-service.svc.cluster.local:8070/"',
+  PRODUCT_API_BASE_URL: '"http://product-service.msa-service.svc.cluster.local:8071/"',
+  RESERVE_API_BASE_URL: '"http://reserve-service.msa-service.svc.cluster.local:8072/"'
 }
