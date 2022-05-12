8bdbe67a8c9f5b83d80eed6d050f987fde0df7ae
chupdachups <chupdachups@gmail.com>
2022-05-12 23:48:14 +0900

    add nginx test

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ACCOUNT_API_BASE_URL: '"http://20.214.184.170:31999/"',
-  PRODUCT_API_BASE_URL: '"http://product-service.msa-service.svc.cluster.local:8071/"',
-  RESERVE_API_BASE_URL: '"http://reserve-service.msa-service.svc.cluster.local:8072/"'
+  ACCOUNT_API_BASE_URL: '"http://20.214.184.170:31070/"',
+  PRODUCT_API_BASE_URL: '"http://20.214.184.170:31071/"',
+  RESERVE_API_BASE_URL: '"http://20.214.184.170:31072/"'
 }
